1d3834a9883353ac068544897da123419fadd2df
Priyanshu Tiwari <priyanshu16181389@gmail.com>
2024-10-08 00:02:38 +0530

    contact page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { FormEventHandler, useState } from 'react'
 
 export const Login = () => {
+  const [state, setState] = useState('Sign Up')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [name, setName] = useState('')
+
+  const onSubmitHandler = async (event: FormEventHandler) => {
+    event.preventDefault()
+  }
   return (
-    <div>Login</div>
+    <form className='min-h-[80vh] flex items-center'>
+      <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg'>
+        <p className='text-2xl font-semibold'>{state === 'Sign Up' ? "Create Account" : "Login"}</p>
+        <p className=''>Please {state === 'Sign Up' ? "sign up" : "log in"} to book appointment </p>
+        {
+          state === 'Sign Up'
+          && <div className='w-full'>
+            <p>Full Name</p>
+            <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="text" onChange={(e) => setName(e.target.value)} value={name} />
+          </div>
+        }
+
+        <div className='w-full'>
+          <p>Email</p>
+          <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="email" onChange={(e) => setEmail(e.target.value)} value={email} />
+        </div>
+        <div className='w-full'>
+          <p>Password</p>
+          <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
+        </div>
+        <button className='bg-primary text-white w-full py-2 rounded-md text-base'>{state === 'Sign Up' ? "Create Account" : "Login"}</button>
+        {
+          state === "Sign Up"
+            ? <p>Already have an account <span className='text-primary underline cursor-pointer' onClick={() => setState('Login')}>Login here</span></p>
+            : <p>Create an new account? <span className='text-primary underline cursor-pointer' onClick={() => setState('Sign Up')}>click here</span></p>
+        }
+      </div>
+    </form>
   )
 }
--- frontend/src/pages/MyProfile.tsx
+++ frontend/src/pages/MyProfile.tsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
 
 export const MyProfile = () => {
+  const [userData, setUserData] = useState({
+    name:"Edward Vincent",
+    image: assets.profile_pic,
+    email: 'richardjameswap@gmail.com',
+    phone: '+1 32566777886',
+    address: {
+      line1: "57th Cross, Richmond",
+      line2: "Circle, Church Road, London"
+    },
+    gender: 'Male',
+    dob: '2002-03-21'
+  })
+
+  const [isEdit, setIsEdit] = useState(false)
   return (
-    <div>MyProfile</div>
+    <div>
+      <img src={userData.image} alt="" />
+      {
+        isEdit ? <input value={userData.name} type="text" onChange={(e) => setUserData(prev => ({...prev, name: e.target.value}))} />
+        : <p>{userData.name}</p>
+      }
+
+      <hr />
+      <div>
+        <p>CONTACT INFORMATION</p>
+        <div></div>
+      </div>
+    </div>
   )
 }
