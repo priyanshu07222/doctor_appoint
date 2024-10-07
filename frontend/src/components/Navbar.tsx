122551f0c9055fd898c08f5b410bce6ca8c57600
Priyanshu Tiwari <priyanshu16181389@gmail.com>
2024-10-08 02:18:08 +0530

    responsive &  appointment page

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -37,14 +37,27 @@ export const Navbar = () => {
                             <div className='absolute top-8 right-0 pt14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
                                 <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
                                     <p className='hover:text-black cursor-pointer' onClick={() => navigate('/my-profile')}>My Profile</p>
-                                    <p className='hover:text-black cursor-pointer' onClick={() => navigate('/my-appoinments')}>My Appointement</p>
+                                    <p className='hover:text-black cursor-pointer' onClick={() => navigate('/my-appointments')}>My Appointement</p>
                                     <p className='hover:text-black cursor-pointer' onClick={() => setToken(false)}>Logout</p>
                                 </div>
                             </div>
                         </div>
                         : <button onClick={() => navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>
                 }
-
+                <img onClick={() => setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon} alt="" />
+                {/*  mobile menu */}
+                <div className={`${showMenu ? 'fixed w-full' : 'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
+                    <div className='flex items-center justify-between px-5 py-6'>
+                        <img className='w-36' src={assets.logo} alt="" />
+                        <img className='w-8' onClick={() => setShowMenu(false)} src={assets.cross_icon} alt="" />
+                    </div>
+                    <ul className='flex flex-col items-center gap-2 font-medium mt-5 px-5 text-lg'>
+                        <NavLink onClick={() => setShowMenu(false)}  to={'/'}><p className='px-4 py-2 rounded inline-block'>Home</p></NavLink>
+                        <NavLink onClick={() => setShowMenu(false)}  to={'/doctors'}><p className='px-4 py-2 rounded inline-block'>ALL DOCTORS</p></NavLink>
+                        <NavLink onClick={() => setShowMenu(false)}  to={'/about'}><p className='px-4 py-2 rounded inline-block'>ABOUT</p></NavLink>
+                        <NavLink onClick={() => setShowMenu(false)}  to={'/contact'}><p className='px-4 py-2 rounded inline-block'>CONTACT</p></NavLink>
+                    </ul>
+                </div>
             </div>
         </div>
     )
