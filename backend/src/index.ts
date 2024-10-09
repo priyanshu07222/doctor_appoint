ad4b2e18ae1cd877e6ce03c76eb9cf2d494ec907
Priyanshu Tiwari <priyanshu16181389@gmail.com>
2024-10-09 16:54:30 +0530

    api addDoctor

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,23 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDb from './config/mongodb';
+import connectCloudinary from './config/cloudinary';
+import adminRouter from './routes/adminRoute';
 
 // app config
 const app = express();
 const port = process.env.PORT || 4000
+connectDb();
+connectCloudinary();
 
 // middlewares
 app.use(express.json())
 app.use(cors())
 
 // api endpoint
+app.use('/api/admin', adminRouter)
+//  localhost:4000/api/admin/add-doctor
 
 app.get('/', (req: Request, res: Response) => {
     res.send('API WORKING ')
